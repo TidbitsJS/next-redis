892456d7d3c5296c2f04d7938ac398befab44a46
Tidbits JS <tidbitsjs@gmail.com>
2022-11-24 08:10:30 +0530

    create index

---
+++ pages/api/create-index.js
@@ -0,0 +1,9 @@
+import { createIndex } from "../../lib";
+
+// one time operation that needs to be done whenever the schema changes
+async function handler(req, res) {
+  await createIndex();
+  res.status(200).send("ok");
+}
+
+export default handler;
